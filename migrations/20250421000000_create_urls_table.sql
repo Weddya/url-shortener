071562ffd42eff0f1dc66a17de1de23a93aef7dc
Aleksander Lepeshkin <ADLepeshkin@1cbit.ru>
2025-04-22 18:05:43 +0300

    fix: disable creating table & index by migration if already exist

--- migrations/20250421000000_create_urls_table.sql
+++ migrations/20250421000000_create_urls_table.sql
@@ -1,12 +1,12 @@
 -- +goose Up
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
                       id SERIAL PRIMARY KEY,
                       original_url TEXT NOT NULL UNIQUE,
                       short_code VARCHAR(10) NOT NULL UNIQUE,
                       created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE INDEX idx_short_code ON urls (short_code);
+CREATE INDEX IF NOT EXISTS idx_short_code ON urls (short_code);
 
 -- +goose Down
 DROP TABLE urls;
