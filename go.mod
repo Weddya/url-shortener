1d4e72b2b4c65aa6b822b9eb268cc97e70b15125
Aleksander Lepeshkin <ADLepeshkin@1cbit.ru>
2025-04-23 18:36:36 +0300

    Add metrics http_request

--- go.mod
+++ go.mod
@@ -9,9 +9,12 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/jxskiss/base62 v1.1.0
 	github.com/labstack/echo/v4 v4.13.3
+	github.com/prometheus/client_golang v1.22.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -22,6 +25,10 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
@@ -30,4 +37,5 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
---
+++ internal/middleware/metrics.go
@@ -0,0 +1,67 @@
+package middleware
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type Metrics struct {
+	HTTPRequestsTotal   *prometheus.CounterVec
+	HTTPRequestDuration *prometheus.HistogramVec
+}
+
+func NewMetrics(namespace string) *Metrics {
+	return &Metrics{
+		HTTPRequestsTotal: promauto.NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: namespace,
+				Name:      "http_requests_total",
+				Help:      "Total number of HTTP requests",
+			},
+			[]string{"method", "path", "status"},
+		),
+		HTTPRequestDuration: promauto.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Namespace: namespace,
+				Name:      "http_request_duration_seconds",
+				Help:      "Duration of HTTP requests",
+				Buckets:   prometheus.DefBuckets,
+			},
+			[]string{"method", "path"},
+		),
+	}
+}
+
+func PrometheusMiddleware(metrics *Metrics) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			start := time.Now()
+			path := c.Path()
+
+			err := next(c)
+			if err != nil {
+				c.Error(err)
+			}
+
+			status := strconv.Itoa(c.Response().Status)
+			duration := time.Since(start).Seconds()
+
+			metrics.HTTPRequestsTotal.WithLabelValues(
+				c.Request().Method,
+				path,
+				status,
+			).Inc()
+
+			metrics.HTTPRequestDuration.WithLabelValues(
+				c.Request().Method,
+				path,
+			).Observe(duration)
+
+			return nil
+		}
+	}
+}
